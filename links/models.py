8ad8e7fe7e153dd98b412d9749d2839145d7544f
nargok <nargok@gmail.com>
2018-12-31 19:53:53 +0900

    Created Link model

--- links/models.py
+++ links/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Link(models.Model):
+  title = models.CharField(max_length=255)
+  url = models.CharField(max_length=255)
+  description = models.CharField(max_length=255)
+
+  def __str__(self):
+    return self.title
