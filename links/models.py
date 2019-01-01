127cf34d598978723b4e1b24a8fadfc6b68a9103
nargok <nargok@gmail.com>
2019-01-01 16:38:35 +0900

    Added sitename & created_at into Link model

--- links/models.py
+++ links/models.py
@@ -4,6 +4,8 @@ class Link(models.Model):
   title = models.CharField(max_length=255)
   url = models.CharField(max_length=255)
   description = models.CharField(max_length=255)
+  site_name = models.CharField(max_length=100, default="")
+  created_at = models.DateTimeField(auto_now_add=True)
 
   def __str__(self):
     return self.title
